b182c16d596f17c3e680820854612dc1e4ac81d6
Meet <meetsabhaya008@gmail.com>
2026-06-23 20:03:24 +1000

    fix: favicon 500px zoom top-left crop to show logo mark clearly

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,13 +1,37 @@
 import { ImageResponse } from "next/og";
+import { readFileSync } from "fs";
+import { join } from "path";
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 export const size = { width: 64, height: 64 };
 export const contentType = "image/png";
 
 export default async function Icon() {
+  const logoBuffer = readFileSync(join(process.cwd(), "public/newonlylogo.png"));
+  const base64 = `data:image/png;base64,${logoBuffer.toString("base64")}`;
+
   return new ImageResponse(
     (
-      <div style={{ width: 64, height: 64, display: "flex" }} />
+      <div
+        style={{
+          width: 64,
+          height: 64,
+          display: "flex",
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+          overflow: "hidden",
+        }}
+      >
+        <img
+          src={base64}
+          style={{
+            width: 500,
+            height: 500,
+            objectFit: "contain",
+            flexShrink: 0,
+          }}
+        />
+      </div>
     ),
     { width: 64, height: 64 }
   );
